572abeb82e66be492bbe1f1a4c01b9b9cfa682a5
behnamizadi <behnamizadi@yahoo.com>
2017-10-10 21:04:28 +0330

    converted to conversation and localization added

--- main.py
+++ main.py
@@ -1,49 +1,109 @@
 # -*- coding: utf-8 -*-
 import logging
 import telegram
+import time
 from telegram import (InlineKeyboardButton,InlineKeyboardMarkup,ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
-ConversationHandler,CallbackQueryHandler)
+	ConversationHandler,CallbackQueryHandler)
+import gettext
+import os
+
+localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
+translate = gettext.translation('messages', localedir, fallback=True)
+_ = translate.gettext
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+START,HAGHIGHI,HAGHIGHI_MABLAGH,PEYVAND1 \
+	,PEYVAND2,PEYVAND1_SALANEH,PEYVAND2_SALANEH,HOGHOOGHI,HOGHOOGHI_ETEBAR  \
+	,HOGHOOGHI_MOSHAREKAT=range(10)
+
+
 context=""
 def start(bot, update):
-	keyboard = [[InlineKeyboardButton("💹تسهیلات با معدل حساب", callback_data='1'),
-    InlineKeyboardButton("❤طرح پیوند مهر", callback_data='2')],
-	[InlineKeyboardButton("💰طرح وثیقه نقدی", callback_data='3')]]
-	reply_markup = InlineKeyboardMarkup(keyboard)
-	update.message.reply_text(
-"""به ربات راهنمای تسهیلات بانک قرض الحسنه مهر ایران خوش آمدید.
-🌹🌹🌹🌹🌹
-\n⚠این ربات هیچ گونه مسولیتی در قبال محاسبات انجام شده ندارد.⚠
-\n⚠ربات در مرحله برنامه نویسی می باشد و اطلاعات آن قابل استناد نیست⚠
-""", reply_markup=reply_markup)
-
-def button(bot, update):
-	global context
-	query = update.callback_query
-	if query.data == '1':
-		context='1'
-		keyboard=[[
-			InlineKeyboardButton('peyvand1',callback_data='11'),
-			InlineKeyboardButton('peyvand2',callback_data='12'),
-			InlineKeyboardButton('peyvand3',callback_data='13')
-		
+	keyboard = [[
+		InlineKeyboardButton(_("haghighi"), callback_data=str(HAGHIGHI)),
+		InlineKeyboardButton(_("hoghooghi"), callback_data=str(HOGHOOGHI))
 		]]
-		reply_markup = InlineKeyboardMarkup(keyboard)
-		bot.send_message(text="context: %s" % context,
-                          chat_id=query.message.chat_id,reply_markup=reply_markup)
+	reply_markup = InlineKeyboardMarkup(keyboard)
+	bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+	time.sleep(1.5)
+	update.message.reply_text(_("wellcome"), reply_markup=reply_markup)
+	return START
+
+def haghighi(bot, update):
+	bot.send_message(text=_("enter_mablagh"),
+                          chat_id=query.message.chat_id)
+                          
+def hoghooghi(bot, update):
+	bot.send_message(text=_("enter_mablagh"),
+                          chat_id=query.message.chat_id)
+
+def peyvand(bot,update):
+	query = update.callback_query
+	bot.send_message(text=query.message.text,
+                          chat_id=query.message.chat_id)
+	                         
                           
 def echo(bot, update):
-	update.message.reply_text(update.message.text)
+	update.message.reply_text(update.message.text,parse_mode=telegram.ParseMode.HTML)
 
-f = open(".token", "r") #opens file with name of "test.txt"
-token=f.read()
-print token
-updater = Updater(token.rstrip("\n\r"))
-f.close()
-updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
-updater.start_polling()
-updater.idle()
+
+def cancel(bot, update):
+	user = update.message.from_user
+	logger.info("User %s canceled the conversation." % user.first_name)
+	update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+	return ConversationHandler.END
+
+def error(bot, update, error):
+	logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+def main():
+	logger.info("starting ... ")
+	#read telegram api token from file
+	f = open(".token", "r") #opens file with name of "test.txt"
+	token=f.read()
+	updater = Updater(token.rstrip("\n\r"))
+	f.close()
+	if(updater):
+		logger.info("token read scuccesfull!")
+
+    # Get the dispatcher to register handlers
+	dp = updater.dispatcher	
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+	conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            START: [MessageHandler(str(HAGHIGHI),haghighi),
+				MessageHandler(str(HOGHOOGHI),hoghooghi)
+            ],
+
+            HAGHIGHI: [MessageHandler(Filters.text, peyvand)],
+
+			HOGHOOGHI: [MessageHandler(Filters.text,error)],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+	dp.add_handler(conv_handler)
+
+    # log all errors
+	dp.add_error_handler(error)
+
+    # Start the Bot
+	updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+	updater.idle()
+
+
+if __name__ == '__main__':
+	main()
